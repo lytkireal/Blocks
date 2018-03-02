849eaca5acbd63fc85bef979a112ea314dc84404
Artem Lytkin <lytkinreal@mail.ru>
2018-03-02 08:06:50 +0300

    Made block initialization

---
+++ Blocks/Blocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocks
+//
+//  Created by macbook air on 02/03/2018.
+//  Copyright © 2018 Lytkin Artem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
